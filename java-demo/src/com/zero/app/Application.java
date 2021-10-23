80078856f0479dc307dc61ab4883b842e4f8134d
zeroHe999 <work_zerohe@yeah.net>
2021-10-23 20:45:54 +0800

    git checkout commit-02

--- java-demo/src/com/zero/app/Application.java
+++ java-demo/src/com/zero/app/Application.java
@@ -9,5 +9,7 @@ public class Application {
     public static void main(String[] args) {
         String dateString = DateUtils.getDateStringByFormatDate(new Date(), DateUtils.DEFAULT_DATE_PATTERN);
         System.out.println(dateString);
+
+        System.out.println("git checkout version-02");
     }
 }
